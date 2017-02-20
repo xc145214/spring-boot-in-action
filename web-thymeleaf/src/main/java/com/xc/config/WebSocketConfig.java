f73193aca31dc54e30f27bbf359e80148c56c218
xc145214 <xc145214@gmail.com>
2017-02-20 17:58:31 +0800

    web socket

---
+++ web-thymeleaf/src/main/java/com/xc/config/WebSocketConfig.java
@@ -0,0 +1,40 @@
+/**
+ * **********************************************************************
+ * HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ * <p>
+ * COPYRIGHT (C) HONGLING CAPITAL CORPORATION 2012
+ * ALL RIGHTS RESERVED BY HONGLING CAPITAL CORPORATION. THIS PROGRAM
+ * MUST BE USED  SOLELY FOR THE PURPOSE FOR WHICH IT WAS FURNISHED BY
+ * HONGLING CAPITAL CORPORATION. NO PART OF THIS PROGRAM MAY BE REPRODUCED
+ * OR DISCLOSED TO OTHERS,IN ANY FORM, WITHOUT THE PRIOR WRITTEN
+ * PERMISSION OF HONGLING CAPITAL CORPORATION. USE OF COPYRIGHT NOTICE
+ * DOES NOT EVIDENCE PUBLICATION OF THE PROGRAM.
+ * HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ * ***********************************************************************
+ */
+package com.xc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+/**
+ *  webSocket 配置。
+ *
+ *  @author xiachuan at 2017/2/16 14:13。
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        stompEndpointRegistry.addEndpoint("/endPointWisely").withSockJS();
+    }
+
+
+
+}
+
